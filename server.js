1817a9c6789c62debb2b31609c318a53fb82250c
mahmoud <mahmoudmosa2877@gmail.com>
2023-02-11 11:53:54 +0200

    creating check function for post

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  const method = req.method;
+  const method = req.method.toLocaleLowerCase();
   const queryString = parsedUrl.query;
-  const header = req.headers;
+  const headers = req.headers;
   const decoder = new stringDecoder("utf-8");
   let buffer = "";
   const choosenHandler =
@@ -24,12 +24,13 @@ const server = http.createServer((req, res) => {
   req.on("end", async () => {
     buffer += decoder.end();
     console.log(buffer, typeof buffer);
+    const bufferJson = JSON.parse(buffer);
     const data = {
       trimmedPath,
       queryString,
       method,
-      header,
-      payload: buffer,
+      headers,
+      payload: bufferJson,
     };
     console.log(data);
 
@@ -38,16 +39,18 @@ const server = http.createServer((req, res) => {
       // console.log(respon, "respone");
       //   const statusCode =
       //     typeof respon.statusCode == "number" ? respon.statusCode : 200;
+      console.log(respon);
       const payload = typeof respon == "object" ? respon : {};
       const payloadString = JSON.stringify(payload);
       res.setHeader("Content-Type", "application/json");
       //   res.writeHead(statusCode);
       console.log(typeof payloadString);
+      console.log(respon, "respon");
       res.end(payloadString);
     } catch (err) {
       // console.log(err, typeof err);
-      // console.log(err);
-      res.end("err");
+      console.log(err, "err in the server");
+      res.end(err);
     }
   });
 });
@@ -60,4 +63,5 @@ const router = {
   sample: handlers.sample,
   users: handlers.users,
   tokens: handlers.tokens,
+  checks: handlers.checks,
 };
