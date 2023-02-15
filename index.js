e8d5fd535c6938851c6ab16afa0fae9d687a182f
mahmoud <mahmoudmosa2877@gmail.com>
2023-02-15 17:40:25 +0200

    making worker out of the server to perform checks

--- index.js
+++ index.js
@@ -8,16 +8,25 @@
 //Dependancies
 const server = require("./lib/server");
 const worker = require("./lib/worker");
+const _data = require("./lib/lib");
+
+_data.list("users");
 
 //Declare the app
 const app = {};
 
 // init function
-app.init = function () {
+app.init = async function () {
   //start the server
   server.init();
   //start the workers
   // worker.init();
+  try {
+    const checks = await worker.performCheck();
+    console.log(checks);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //Execute
