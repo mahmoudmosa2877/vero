799c777190b1e10ffc618c5e5ec6049ff032b248
mahmoud <mahmoudmosa2877@gmail.com>
2023-02-26 21:34:00 +0200

    initial commit for command line tool designed by event handler

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 //Dependancies
 const server = require("./lib/server");
 const worker = require("./lib/worker");
+const cli = require("./lib/cli");
 
 //Declare the app
 const app = {};
@@ -19,6 +20,10 @@ app.init = async function () {
 
   //start the workers
   // worker.init();
+  // Start the CLI, but make sure it starts last
+  setTimeout(function () {
+    cli.init();
+  }, 50);
 };
 
 //Execute
